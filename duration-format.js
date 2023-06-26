4d0dd1687421bb0f13ed56f9d54f356c476b5211
Tom Carmichael-Mhanna <tomcarmichael@hotmail.co.uk>
2023-06-26 15:37:00 +0100

    Refactor code for brevity

--- duration-format.js
+++ duration-format.js
@@ -1,64 +1,33 @@
 function formatDuration(inputSeconds) {
-  if (inputSeconds < 0 || typeof inputSeconds != 'number') {
+   if (inputSeconds < 0 || typeof inputSeconds != 'number') {
     return 'Argument must be a non-negative integer.';
   } else if (inputSeconds === 0) {
     return 'now'; 
   }
 
-  const secondsPerYear = 31536000; // 60 * 60 * 24 * 365
-  const secondsPerDay = 86400; // 60 * 60 * 24
-  const secondsPerHour = 3600; // 60 * 60
-
-  const years = Math.floor(inputSeconds / secondsPerYear);
-  const days = (Math.floor(inputSeconds / secondsPerDay) % 365);
-  const hours = (Math.floor(inputSeconds / secondsPerHour) % 24);
-  const minutes = (Math.floor(inputSeconds / 60) % 60);
-  const remainderSeconds = inputSeconds % 60;
-  
-  let formattedDuration = "";
-
-  if (years) {
-    formattedDuration += `${years} year${years > 1 ? 's' : ''}`;
-    if ((days && (hours || minutes || remainderSeconds)) || 
-        (hours && (minutes || remainderSeconds)) ||
-        (minutes && remainderSeconds)) {
-          formattedDuration += `, `;
-        }
-    if ((days && !hours && !minutes && !remainderSeconds) || 
-        (hours && !days && !minutes && !remainderSeconds) || 
-        (minutes && !days && !hours && !remainderSeconds) ||
-        (remainderSeconds && !days && !hours && !minutes)) { 
-          formattedDuration += ` and `;
-        }
-  } 
-
-  if (days) { 
-    formattedDuration += `${days} day${days > 1 ? 's' : ''}`;
-    if ((hours && (minutes || remainderSeconds)) || (minutes && remainderSeconds)) {
-          formattedDuration += `, `;
-        }
-    if ((hours && !minutes && !remainderSeconds) || (minutes && !hours && !remainderSeconds) ||
-        (remainderSeconds  && !hours && !minutes)) {
-          formattedDuration += ` and `;
-        }
+  const divisions = {
+    year: 31536000, // 60 * 60 * 24 * 365
+    day: 86400, // 60 * 60 * 24
+    hour: 3600, // 60 * 60
+    minute: 60,
+    second: 1
   }
 
-  if (hours) {
-    formattedDuration += `${hours} hour${hours > 1 ? 's' : ''}`;
-    if (minutes && remainderSeconds) { formattedDuration += `, ` }
-    if ((minutes && !remainderSeconds) || (remainderSeconds && !minutes)) { formattedDuration += ` and ` }
+  let durationsArray = [];
+
+  for (key in divisions) {
+    if (inputSeconds >= divisions[key]) {
+      let val = Math.floor(inputSeconds / divisions[key])
+      durationsArray.push(val += val > 1 ? ' ' + key + 's' : ' ' + key); 
+      inputSeconds = inputSeconds % divisions[key];
+    }
   }
 
-  if (minutes) { 
-    formattedDuration += `${minutes} minute${minutes > 1 ? 's' : ''}`;
-    if (remainderSeconds) { formattedDuration += ` and ` }
-  }
+  if (durationsArray.length === 1) { return durationsArray[0] }
 
-  if (remainderSeconds) {
-    formattedDuration += `${remainderSeconds} second${remainderSeconds > 1 ? 's' : ''}`;
-  }
-
-  return formattedDuration;
+  let commaSeparatedDurations = durationsArray.slice(0, -1).join(', ');
+  const lastElement = durationsArray[durationsArray.length - 1];
+  return commaSeparatedDurations += ` and ${lastElement}`;
 }
 
 module.exports = formatDuration;
